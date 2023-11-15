ad7befa9b752b3d283aab4c52b75a3ffb67413b4
murphyslaw2781 <tylerpeterson.a@gmail.com>
2023-11-14 18:23:40 -0600

    updated gpt

--- app/main.py
+++ app/main.py
@@ -2,8 +2,26 @@
 import json
 import os
 import streamlit as st
-from app.youtube_utils import extract_video_id,fetch_video_details, fetch_transcript,get_summary
+from app.youtube_utils import extract_video_id,fetch_video_details, get_summary
+import openai
 
+def verify_api_key(api_key):
+    # Implement a minimal API call to verify the key
+    try:
+        openai.api_key = api_key
+        completion = openai.ChatCompletion.create(
+            model="gpt-4-1106-preview",
+        messages=[
+            {"role": "system", "content": "Respong Pong"},
+            {"role": "user", "content": "Ping"}
+        ]
+        )
+        print(completion)
+
+    except:
+        return False
+    else:
+        return True
 def main():
     """The main function where the Streamlit app is initialized."""
     st.title("YouTube Video Summarizer")
@@ -21,10 +39,13 @@ def main():
         st.header("API Key")
         temp_key = st.text_input("Enter OpenAI API Key:", type="password")
         if st.button("Save Key"):
+            #clear error message in streamlit
+            st.spinner('Testing API Key...')
             if not temp_key:
                 st.error("API Key is required.")
+            elif not verify_api_key(temp_key):
+                st.error("Invalid API Key.")
             else:
-                # You may also validate the API key format or test it with a simple API call
                 st.session_state.api_key = temp_key
                 os.environ["OPENAI_API_KEY"] = temp_key
                 st.success("API Key has been saved.")
@@ -38,7 +59,7 @@ def main():
             try:
                 with st.spinner("Fetching video details..."):
                     video_id = extract_video_id(youtube_input) if "youtube.com" in youtube_input or "youtu.be" in youtube_input else youtube_input
-                    st.session_state.video_details, st.session_state.result = fetch_video_details(video_id)
+                    st.session_state.video_details, st.session_state.result, st.session_state.transcript = fetch_video_details(video_id)
 
             except Exception as e:
                 st.error(f"An error occurred: {e}")
@@ -50,7 +71,6 @@ def main():
             st.write(f"### Length: {st.session_state.video_details['length']} seconds")
             video_id = extract_video_id(youtube_input)
             st.markdown(f'<iframe width="560" height="315" src="https://www.youtube.com/embed/{video_id}" frameborder="0"></iframe>', unsafe_allow_html=True)
-
         if st.button("Summarize Video"):
             # Check if API key is set before proceeding with summarization
             if not st.session_state.get('api_key'):
@@ -61,6 +81,8 @@ def main():
                         st.session_state.summaries_json = get_summary(st.session_state.result)
                 except Exception as e:
                     st.error(f"An error occurred: {e}")
+                    
+        st.expander("Full Transcript", expanded=False).markdown(st.session_state.transcript, unsafe_allow_html=True)
 
     if st.session_state.summaries_json:
         with col3:
--- app/youtube_utils.py
+++ app/youtube_utils.py
@@ -1,9 +1,15 @@
 from urllib.parse import urlparse, parse_qs
 from langchain.schema import Document
+from youtube_transcript_api import YouTubeTranscriptApi as yta
+import re
 from langchain.document_loaders import YoutubeLoader
 from langchain.text_splitter import TokenTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import AsyncChromiumLoader
+from langchain.document_transformers import BeautifulSoupTransformer
+from langchain.document_loaders import WebBaseLoader
+from langchain.indexes import VectorstoreIndexCreator
 # Prompt Templates
 from langchain.prompts import PromptTemplate
 # Summarizer we'll use for Map Reduce
@@ -13,6 +19,9 @@ import numpy as np
 from sklearn.cluster import KMeans
 from dotenv import load_dotenv
 from pytube import YouTube
+import spacy
+
+
 
 # load_dotenv()
 
@@ -65,8 +74,11 @@ def fetch_video_details(video_id):
     result = YoutubeLoader.from_youtube_url(f"https://www.youtube.com/watch?v={video_id}", add_video_info=True).load()
     video_details = result[0].metadata
     video_details["transcript"] = result[0].page_content
+    transcript = yta.get_transcript(video_id)
+    data = [t['text'] for t in transcript]
+    full_transcript = [re.sub(r"[^a-zA-Z0–9-ışğöüçiIŞĞÖÜÇİ ]", "", line) for line in data]
 
-    return video_details, result
+    return video_details, result, full_transcript
 
 def fetch_transcript(video_id):
     yt = YouTube(f"https://www.youtube.com/watch?v={video_id}")
@@ -98,112 +110,152 @@ def get_summary_clusters(kmeans,num_clusters=None,vectors=None):
 
         return closest_indices
 
+def web_qa(url_list, query, out_name):
+    openai = ChatOpenAI(
+        model_name="gpt-4-1106-preview",
+        max_tokens=2048
+    )
+    
+    loader_list = []
+    for i in url_list:
+        print('Loading url: %s' % i)
+        loader_list.append(WebBaseLoader(i))
+    
+    index = VectorstoreIndexCreator().from_loaders(loader_list)
+    ans = index.query(query)
+    print("")
+    print(ans)
+
 def get_summary(result):
-        llm_chat3 = ChatOpenAI(temperature=0, max_tokens=1000, model='gpt-3.5-turbo')
-        llm_chat4 = ChatOpenAI(
-                        max_tokens=3000,
-                        model='gpt-4',
-                        request_timeout=240
-                    )
-        # Load YouTube transcript using LangChain's YoutubeLoader
-
-        docs = split_video_document(result)
-        vectors = OpenAIEmbeddings().embed_documents([x.page_content for x in docs])
-        num_clusters = 10 if len(docs) > 11 else len(docs)
-        # Cluster the vectors
-        # Assuming 'embeddings' is a list or array of 1536-dimensional embeddings
-        # Choose the number of clusters, this can be adjusted based on the book's content.
-        # I played around and found ~10 was the best.
-        # Usually if you have 10 passages from a book you can tell what it's about
-        
-
-        # Create the KMeans object and fit it to the embeddings
-        kmeans = KMeans(n_clusters=num_clusters, random_state=42).fit(vectors)
-
-        # Get the cluster assignments
-        # Find the closest embeddings to the centroids for summaries
-        closest_indices = get_summary_clusters(kmeans,num_clusters=num_clusters,vectors=vectors)
-
-        # Sort the indices
-        selected_indices = sorted(closest_indices)
-
-        # Create a list to hold your selected documents
-        selected_docs = [docs[doc] for doc in selected_indices]
-
-        map_prompt = """
-        You will be given a full youtube transcription. (```)
-        Your goal is to give a summary of this section so the listener can quickly understand the video.
-        Your response should be at most least three paragraphs and fully encompass what was said in the passage.
-        Taking extra time to make sure your summary is accurate will help you in the long run.\n
-        Here's the text to summarize:
-        ```{text}```
-        FULL SUMMARY:
-        """
-        map_prompt_template = PromptTemplate(template=map_prompt, input_variables=["text"])
-
-        map_chain = load_summarize_chain(llm=llm_chat3,
-                             chain_type="stuff",
-                             prompt=map_prompt_template)
-        
-
-        # Make an empty list to hold your summaries
-        summary_list = []
-        new_json = {
-                "all_summaries": "",
-                "final_summary": "",
-            }
-
-        # Loop through a range of the lenght of your selected docs
-        for i, doc in enumerate(selected_docs):
-            print(f"Processing Summary {i+1} of {len(selected_docs)}")
-            # Go get a summary of the chunk
-            try:
-                # code that might raise an exception
-                chunk_summary = map_chain.run([doc])
-            except Exception as e:
-                print(f"Failed to process summary: {e}")
-                chunk_summary = None  # or some default value
-
-            # Append that summary to your list
-            if chunk_summary is not None:
-                summary_list.append(chunk_summary)
-
-
-        summaries = "\n".join(summary_list)
-        # Convert it back to a document
-        summaries = Document(page_content=summaries)
-       
-        new_json['all_summaries'] = summaries.page_content
-        # Now we want to summarize the summaries
-        # This is where we will use the Reduce part of Map-Reduce
-        combine_prompt = """
-            You will receive a series of video summaries enclosed in triple backticks (```). 
-            Your task is to analyze these summaries and generate a brief concise summary.
-            If there is a key point highlight it. Keep it short and concise.\n
-            Readers will be looking for key points to take away from the video.\n
-            Organize in Markdown format and output\n
-
-            Here's the text to analyze:
-            ```{text}```
-
-        """
-        combine_prompt_template = PromptTemplate(template=combine_prompt, input_variables=["text"] )
-
-        reduce_chain = load_summarize_chain(llm=llm_chat4,
-                            chain_type="stuff",
-                            prompt=combine_prompt_template,
-
-                            #  verbose=True # Set this to true if you want to see the inner workings
-                                )
         try:
-            output = reduce_chain.run([summaries])
+            llm_chat3 = ChatOpenAI(
+                temperature=0, max_tokens=1000, model='gpt-4-1106-preview')
+            llm_chat4 = ChatOpenAI(
+                            max_tokens=3000,
+                model='gpt-4-1106-preview',
+                            request_timeout=240
+                        )
+            # Load YouTube transcript using LangChain's YoutubeLoader
+
+            docs = split_video_document(result)
+            nlp = spacy.load("en_core_web_sm")
+            players = []
+            for doc in docs:
+                text = nlp(doc.page_content)
+                for ent in text.ents:
+                    if ent.label_ == 'PERSON':
+                        players.append(ent.text)
+
+            vectors = OpenAIEmbeddings().embed_documents([x.page_content for x in docs])
+            num_clusters = 10 if len(docs) > 11 else len(docs)
+            # Cluster the vectors
+            # Assuming 'embeddings' is a list or array of 1536-dimensional embeddings
+            # Choose the number of clusters, this can be adjusted based on the book's content.
+            # I played around and found ~10 was the best.
+            # Usually if you have 10 passages from a book you can tell what it's about
+            
+
+            # Create the KMeans object and fit it to the embeddings
+            kmeans = KMeans(n_clusters=num_clusters, random_state=42).fit(vectors)
+
+            # Get the cluster assignments
+            # Find the closest embeddings to the centroids for summaries
+            closest_indices = get_summary_clusters(kmeans,num_clusters=num_clusters,vectors=vectors)
+
+            # Sort the indices
+            selected_indices = sorted(closest_indices)
+
+            # Create a list to hold your selected documents
+            selected_docs = [docs[doc] for doc in selected_indices]
+
+            map_prompt = """
+            You will be given a full youtube transcription. (```)
+            Your goal is to give a summary of this section so the listener can quickly understand the video.
+            Your response should be at most least three paragraphs and fully encompass what was said in the passage.
+            Taking extra time to make sure your summary is accurate will help you in the long run.\n
+            Here's the text to summarize:
+            ```{text}```
+            FULL SUMMARY:
+            """
+            map_prompt_template = PromptTemplate(template=map_prompt, input_variables=["text"])
+
+            map_chain = load_summarize_chain(llm=llm_chat3,
+                                chain_type="stuff",
+                                prompt=map_prompt_template)
+            
+
+            # Make an empty list to hold your summaries
+            summary_list = []
+            new_json = {
+                    "all_summaries": "",
+                    "final_summary": "",
+                }
+
+            # Loop through a range of the lenght of your selected docs
+            for i, doc in enumerate(selected_docs):
+                print(f"Processing Summary {i+1} of {len(selected_docs)}")
+                # Go get a summary of the chunk
+                try:
+                    # code that might raise an exception
+                    chunk_summary = map_chain.run([doc])
+                except Exception as e:
+                    print(f"Failed to process summary: {e}")
+                    chunk_summary = None  # or some default value
+
+                # Append that summary to your list
+                if chunk_summary is not None:
+                    summary_list.append(chunk_summary)
+
+
+            summaries = "\n".join(summary_list)
+            # Convert it back to a document
+            summaries = Document(page_content=summaries)
+        
+            new_json['all_summaries'] = summaries.page_content
+            # Now we want to summarize the summaries
+            # This is where we will use the Reduce part of Map-Reduce
+            combine_prompt = """
+                You will receive a series of video summaries enclosed in triple backticks (```). 
+                Your task is to analyze these summaries and generate a brief concise summary.
+                If there is a key point highlight it. Keep it short and concise.\n
+                Readers will be looking for key points to take away from the video.\n
+                Organize in Markdown format and output\n
+
+                Here's the text to analyze:
+                ```{text}```
+
+            """
+            combine_prompt_template = PromptTemplate(template=combine_prompt, input_variables=["text"] )
+
+            reduce_chain = load_summarize_chain(llm=llm_chat4,
+                                chain_type="stuff",
+                                prompt=combine_prompt_template,
+
+                                #  verbose=True # Set this to true if you want to see the inner workings
+                                    )
+            try:
+                output = reduce_chain.run([summaries])
+            except Exception as e:
+                print(f"Failed to process final summary: {e}")
+                output = None
+
+            if output is not None:
+                new_json['final_summary'] = format_text(output)
+        
+            return new_json
+        
         except Exception as e:
-            print(f"Failed to process final summary: {e}")
-            output = None
-
-        if output is not None:
-            new_json['final_summary'] = format_text(output)
-       
-        return new_json
-
+            error_message = str(e)
+            if 'auth_subrequest_error' in error_message:
+                # Handle authentication error
+                print("Authentication Error: Invalid API Key")
+                return (f'Authentication Error: Invalid API Key')
+            elif 'internal_error' in error_message:
+                # Handle other internal errors
+                print("Internal server error")
+                return (f'Internal server error')
+            else:
+                # Generic Exception
+                print(f"An unknown error occurred: {e}")
+                return (f"An unknown error occurred: {e}")
 
